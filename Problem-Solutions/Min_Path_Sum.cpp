1994731568d33785e3089816a0a3292be3739946
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-03-06 22:25:36 +0530

    comments to min sum path sol

--- Problem-Solutions/Min_Path_Sum.cpp
+++ Problem-Solutions/Min_Path_Sum.cpp
@@ -7,8 +7,8 @@ public:
     int minPathSum(vector<vector<int>> &grid)
     {
 
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size();    // number of rows
+        int m = grid[0].size(); // number of columns
 
         for (int i = 0; i < n; i++)
         {
@@ -16,13 +16,13 @@ public:
             {
                 if (i > 0 && j > 0)
                 {
-                    grid[i][j] = grid[i][j] + min(grid[i - 1][j], grid[i][j - 1]);
+                    grid[i][j] = grid[i][j] + min(grid[i - 1][j], grid[i][j - 1]); // since we can move either right col or down row
                 }
-                else if (i > 0)
+                else if (i > 0) // when j = 0 then we cant get j-1 index since it is out of bounds
                 {
                     grid[i][j] = grid[i][j] + grid[i - 1][j];
                 }
-                else if (j > 0)
+                else if (j > 0) // when i = 0 then we cant get j-1 index since it is out of bounds
                 {
                     grid[i][j] = grid[i][j] + grid[i][j - 1];
                 }
