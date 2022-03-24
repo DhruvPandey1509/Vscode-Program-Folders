162d44eaff4ac3a10fc446d172b81ffea864482c
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-03-24 15:32:18 +0530

    Is Subsequence Solution another way

--- Problem-Solutions/is_Subsequence.cpp
+++ Problem-Solutions/is_Subsequence.cpp
@@ -49,3 +49,29 @@ public:
         return false;
     }
 };
+
+class Solution1
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        int i = 0, j = 0;
+        int n1 = s.length(), n2 = t.length();
+
+        while (i < n1 && j < n2)
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+
+        if (i == n1) // if the iterator of s is of its length that means s is subsequence of t;
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
