1a05c4fe010055a517dc12263fcc6aac880d7c51
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-03-26 16:20:09 +0530

    Maximum Product Subarray Solution

---
+++ Problem-Solutions/Maximum_Product_Subarray.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+        int maxP = 0;  // maximum Product
+        int currP = 1; // current Product
+        for (int i = 0; i < nums.size(); i++)
+        {
+
+            currP *= nums[i];
+            maxP = max(maxP, currP);
+            if (currP == 0)
+            {
+                currP = 1;
+            }
+        }
+
+        currP = 1;
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+
+            currP *= nums[i];
+            maxP = max(maxP, currP);
+            if (currP == 0)
+            {
+                currP = 1;
+            }
+        }
+
+        return maxP;
+    }
+};
