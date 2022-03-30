08e4861eda74ca8c9f3ba8c75541e028ab97f182
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-03-31 01:48:11 +0530

    Isomorphic Strings Solution

---
+++ Problem-Solutions/Isomorphic_Strings.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    bool isIsomorphic(string s, string t)
+    {
+
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+
+        unordered_map<char, char> map1;
+        unordered_map<char, bool> map2;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch1 = s[i];
+            char ch2 = t[i];
+
+            if (map1.find(ch1) != map1.end())
+            {
+                if (map1[ch1] != ch2)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (map2.find(ch2) != map2.end())
+                {
+                    return false;
+                }
+                else
+                {
+
+                    map1.insert({ch1, ch2});
+                    map2.insert({ch2, true});
+                }
+            }
+        }
+        return true;
+    }
+};
