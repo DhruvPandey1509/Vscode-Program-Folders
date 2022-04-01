b69b7e135b5d09f37668de6c11e9c8686eed26ee
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-04-01 14:55:59 +0530

    Reverse String Solution

---
+++ Problem-Solutions/Reverse_String.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution
+{
+public:
+    void swap(char *a, char *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    void reverseString(vector<char> &s)
+    {
+
+        // reverse(s.begin(), s.end());
+
+        for (int i = 0; i < s.size() / 2; i++)
+        {
+            swap(&s[i], &s[s.size() - 1 - i]);
+        }
+    }
+};
