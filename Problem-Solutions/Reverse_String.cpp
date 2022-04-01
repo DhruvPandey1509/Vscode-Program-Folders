4e6f7ace3b60bfa8abf383bf3d2d3274f93d45c6
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-04-01 18:51:46 +0530

    Contains Duplicate II Solution

--- Problem-Solutions/Reverse_String.cpp
+++ Problem-Solutions/Reverse_String.cpp
@@ -11,9 +11,7 @@ public:
     }
     void reverseString(vector<char> &s)
     {
-
         // reverse(s.begin(), s.end());
-
         for (int i = 0; i < s.size() / 2; i++)
         {
             swap(&s[i], &s[s.size() - 1 - i]);
