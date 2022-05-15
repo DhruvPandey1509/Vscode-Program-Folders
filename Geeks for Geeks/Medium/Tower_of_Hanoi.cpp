90c564dd66eff96a86ca107943052b24914c6c45
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-05-16 00:33:52 +0530

    Tower of Hanoi

---
+++ Geeks for Geeks/Medium/Tower_of_Hanoi.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    // You need to complete this function
+
+    // avoid space at the starting of the string in "move disk....."
+    long long count = 0;
+    long long toh(int n, int from, int to, int aux)
+    {
+        if (n > 0)
+        {
+            toh(n - 1, from, aux, to);
+            cout << "move disk " << n << " from rod " << from << " to rod " << to << endl;
+            count++;
+            toh(n - 1, aux, to, from);
+        }
+
+        return count;
+    }
+};
