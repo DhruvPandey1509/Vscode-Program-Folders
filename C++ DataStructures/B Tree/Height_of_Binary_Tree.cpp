f8869cbff093b33d200cba72967630fadcc1a140
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-05-22 00:28:57 +0530

    Height of Binary tree

---
+++ C++ DataStructures/B Tree/Height_of_Binary_Tree.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to find the height of a binary tree.
+    int height(struct Node *node)
+    {
+        if (node == NULL)
+        {
+            return 0;
+        }
+
+        int lh = height(node->left);
+        int rh = height(node->right);
+
+        return max(lh, rh) + 1;
+    }
+};
