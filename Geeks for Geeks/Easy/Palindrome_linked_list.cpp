b44281c3c7674db2ff77cae949e48574eb3b5480
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-05-28 02:12:07 +0530

    .

--- Geeks for Geeks/Easy/Palindrome_linked_list.cpp
+++ Geeks for Geeks/Easy/Palindrome_linked_list.cpp
@@ -66,3 +66,69 @@ public:
         return true;
     }
 };
+
+// class Solution{
+//   public:
+//   Node *reverse(Node *head)
+//     {
+//         if (head == NULL || head->next == NULL)
+//         {
+//             return head;
+//         }
+
+//         Node *prev = NULL;
+//         Node *curr = head;
+//         Node *nxt = head;
+
+//         while (curr != NULL)
+//         {
+//             nxt = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = nxt;
+//         }
+//         return prev;
+//     }
+//    int countNodes(Node *head)
+//     {
+//         int count = 0;
+//         while(head != NULL)
+//         {
+//             count++;
+//             head = head->next;
+//         }
+
+//         return count;
+//     }
+//     //Function to check whether the list is palindrome.
+//     bool isPalindrome(Node *head)
+//     {
+//         if(head->next == NULL) return true;
+
+//         int count = countNodes(head);
+
+//         Node* temp = head;
+//         int k = 1;
+//             while(temp != NULL)
+//             {
+//                 k++;
+//                 temp = temp->next;
+//                 if(k == count/2)
+//                 {
+//                     temp->next = reverse(temp->next);
+//                     break;
+//                 }
+//             }
+//             Node *rt = temp->next;
+//             temp = head;
+//             while(rt != NULL)
+//             {
+//                 if(temp->data != rt->data) return false;
+
+//                 temp = temp->next;
+//                 rt = rt->next;
+//             }
+
+//             return true;
+//     }
+// };
