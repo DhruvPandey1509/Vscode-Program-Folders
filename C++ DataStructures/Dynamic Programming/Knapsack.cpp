0a6909c88141072c4d27748202eb5cf18ee2bcb0
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-06-07 16:02:53 +0530

    Knapsack approach 2

--- C++ DataStructures/Dynamic Programming/Knapsack.cpp
+++ C++ DataStructures/Dynamic Programming/Knapsack.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+// Recursive Approach 1
 int knapsack(int *wts, int *prices, int n, int w)
 {
     if (n == 0 || w == 0)
@@ -18,6 +18,22 @@ int knapsack(int *wts, int *prices, int n, int w)
     return max(inc, exc);
 }
 
+// Recursive Approach 2
+int knapsack2(int wts[], int prices[], int n, int W)
+{
+    if (n == 0 || W == 0)
+    {
+        return 0;
+    }
+
+    if (wts[n - 1] >= W)
+    {
+        return knapsack2(wts, prices, n - 1, W);
+    }
+
+    return max(knapsack2(wts, prices, n - 1, W), prices[n - 1] + knapsack2(wts, prices, n - 1, W - wts[n - 1]));
+}
+
 int main()
 {
     int n = 4, w = 11;
@@ -25,6 +41,6 @@ int main()
     int prices[] = {2, 20, 20, 10};
 
     cout << knapsack(wts, prices, n, w) << endl;
-  
+
     return 0;
 }
