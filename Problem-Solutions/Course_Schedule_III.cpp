9fe16ebae2f589e5d9eec26aa0ba005c41b6790d
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-06-24 02:52:47 +0530

    Course Schedule III added simpler code

--- Problem-Solutions/Course_Schedule_III.cpp
+++ Problem-Solutions/Course_Schedule_III.cpp
@@ -38,3 +38,36 @@ public:
         return count;
     }
 };
+
+//Same Way but simpler code
+class Solution1 {
+public:
+    
+    static bool sortFunc(vector<int> &a, vector<int> &b)
+    {
+        return a[1] < b[1];
+    }
+    
+    int scheduleCourse(vector<vector<int>>& courses) {
+
+        sort(courses.begin(), courses.end(), sortFunc);
+        
+        priority_queue<int> pq;
+        int time = 0;
+        
+        for(auto c : courses)
+        {
+            time += c[0];
+            pq.push(c[0]);
+            
+            if(time > c[1])
+            {
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+        
+        return pq.size();
+        
+    }
+};
