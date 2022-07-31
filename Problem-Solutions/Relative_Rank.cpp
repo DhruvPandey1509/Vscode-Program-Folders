66e95fadc5cb37788ed2f96af03cd2489d699198
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-07-31 18:04:37 +0530

    Relative Rank

---
+++ Problem-Solutions/Relative_Rank.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> findRelativeRanks(vector<int> &score)
+    {
+
+        int n = score.size();
+        if (n == 1)
+            return {"Gold Medal"};
+
+        if (n == 2)
+        {
+            vector<string> res(2);
+            if (score[0] > score[1])
+            {
+                res[0] = "Gold Medal";
+                res[1] = "Silver Medal";
+            }
+            else
+            {
+                res[1] = "Gold Medal";
+                res[0] = "Silver Medal";
+            }
+
+            return res;
+        }
+
+        vector<pair<int, int>> v; // first = score[index], second = index
+        for (int i = 0; i < n; i++)
+        {
+            v.push_back({score[i], i});
+        }
+
+        sort(v.begin(), v.end());
+        reverse(v.begin(), v.end());
+
+        vector<string> res(n);
+
+        res[v[0].second] = "Gold Medal";
+        res[v[1].second] = "Silver Medal";
+        res[v[2].second] = "Bronze Medal";
+
+        for (int i = 3; i < n; i++)
+        {
+            res[v[i].second] = to_string(i + 1);
+        }
+
+        return res;
+    }
+};
