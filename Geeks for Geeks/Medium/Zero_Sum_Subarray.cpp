e5f10f7da0d40bca88764da051c048531c986dc1
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-08-03 00:05:21 +0530

    Zero Sum Subarrays

---
+++ Geeks for Geeks/Medium/Zero_Sum_Subarray.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to count subarrays with sum equal to 0.
+    ll findSubarray(vector<ll> arr, int n)
+    {
+        vector<int> pre(n + 1, 0);
+        unordered_map<int, int> m;
+        m[0] = 1;
+        int res = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            pre[i + 1] = pre[i] + arr[i];
+            if (m.find(pre[i + 1]) != m.end())
+            {
+                res += m[pre[i + 1]];
+            }
+            m[pre[i + 1]]++;
+        }
+
+        return res;
+    }
+};
