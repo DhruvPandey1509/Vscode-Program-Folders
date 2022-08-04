be786385b5113e9f6c0eb89664588399e111b586
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-08-04 22:02:31 +0530

    Check if Number Has Equal Digit Count and Digit Value

---
+++ Problem-Solutions/Check_if_Number_has_Equal_Digit_Count_and_Digit_Value.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    bool digitCount(string num)
+    {
+        map<char, int> m;
+        for (int i = 0; i < num.size(); i++)
+        {
+            m[num[i]]++;
+        }
+        for (int i = 0; i < num.size(); i++)
+        {
+            if (m['0' + i] != num[i] - '0')
+                return false;
+        }
+        return true;
+    }
+};
