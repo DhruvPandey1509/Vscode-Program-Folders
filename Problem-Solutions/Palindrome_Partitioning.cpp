859c04005a4193a5becad0274ea56ccfe5dcefb1
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-08-26 12:21:05 +0530

    Palindrome Partitioning

---
+++ Problem-Solutions/Palindrome_Partitioning.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<string>> res;
+    bool isPalin(string s, int st, int en)
+    {
+        while (st <= en)
+        {
+            if (s[st] != s[en])
+                return false;
+
+            st++;
+            en--;
+        }
+        return true;
+    }
+
+    void makePartition(vector<string> v, string s, int start)
+    {
+        if (start == s.length())
+        {
+            res.push_back(v);
+            return;
+        }
+
+        for (int i = start; i < s.size(); i++)
+        {
+            if (isPalin(s, start, i))
+            {
+                v.push_back(s.substr(start, i - start + 1));
+                makePartition(v, s, i + 1);
+                v.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s)
+    {
+        vector<string> v;
+        makePartition(v, s, 0);
+        return res;
+    }
+};
