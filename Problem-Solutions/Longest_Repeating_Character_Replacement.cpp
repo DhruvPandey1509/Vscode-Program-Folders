2e43a641b67addfbc9856ad5926f563ff393d147
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-08-28 19:19:05 +0530

    Longest Repeating Character Replacement

--- Problem-Solutions/Longest_Repeating_Character_Replacement.cpp
+++ Problem-Solutions/Longest_Repeating_Character_Replacement.cpp
@@ -6,7 +6,6 @@ class Solution
 public:
     int characterReplacement(string s, int k)
     {
-
         int right = 0, left = 0, maxLetter = 0;
         vector<int> v(26, 0);
         while (right < s.length())
@@ -22,5 +21,36 @@ public:
             }
         }
         return right - left;
+    }
+};
+class Solution2
+{
+public:
+    int characterReplacement(string s, int k)
+    {
+        int res = 0;
+        unordered_map<int, int> m;
+        int maxFreq = 0;
+        int st = 0;
+        int en = 0;
+        while (en < s.size())
+        {
+            m[s[en]]++;
+            maxFreq = max(maxFreq, m[s[en]]);
+
+            int currLen = en - st + 1;
+
+            if (currLen - maxFreq > k)
+            {
+                m[s[st]]--;
+                st++;
+            }
+
+            currLen = en - st + 1; // in case st is increased
+            res = max(res, currLen);
+            en++;
+        }
+
+        return res;
     }
 };
