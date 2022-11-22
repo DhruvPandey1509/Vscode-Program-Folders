05e7f9dcb0402b82f0288696a50e0f950ae4cf75
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-11-22 18:57:59 +0530

    Bipartite Graph

---
+++ Geeks for Geeks/Medium/Bipartite_Graph.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution
+{
+public:
+    bool check(vector<int> adj[], vector<int> &color, int start, int v)
+    {
+        queue<int> q;
+        q.push(start);
+        color[start] = 1;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (auto nbr : adj[node])
+            {
+                if (color[nbr] == -1)
+                {
+                    q.push(nbr);
+                    color[nbr] = !color[node];
+                }
+                else if (color[nbr] == color[node])
+                    return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(int V, vector<int> adj[])
+    {
+        // Code here
+        vector<int> color(V, -1);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1)
+            {
+                if (!check(adj, color, i, V))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
