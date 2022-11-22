c0686a4a4bb7cae1d746a34ab2ff66656529e2df
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-11-22 19:37:06 +0530

    Bipartite Graph DFS

--- Geeks for Geeks/Medium/Bipartite_Graph.cpp
+++ Geeks for Geeks/Medium/Bipartite_Graph.cpp
@@ -1,6 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
-
+// BFS
 class Solution
 {
 public:
@@ -36,6 +36,43 @@ public:
             if (color[i] == -1)
             {
                 if (!check(adj, color, i, V))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
+// DFS
+class Solution
+{
+public:
+    bool dfs(vector<int> adj[], vector<int> &color, int node, int col)
+    {
+        color[node] = col;
+        for (auto nbr : adj[node])
+        {
+            if (color[nbr] == -1)
+            {
+                if (!dfs(adj, color, nbr, !col))
+                    return false;
+            }
+            else if (color[nbr] == color[node])
+                return false;
+        }
+
+        return true;
+    }
+    bool isBipartite(int V, vector<int> adj[])
+    {
+        // Code here
+        vector<int> color(V, -1);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1)
+            {
+                if (!dfs(adj, color, i, 0))
                     return false;
             }
         }
