5c74577d6bc6ca4868b1ed3e4c103fe8ccd07f56
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-11-26 20:19:03 +0530

    Construct Binary Tree from String with bracket representation

---
+++ Geeks for Geeks/Medium/Construct_Binary_Tree_from_String_with_bracket_representation.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+using namespace std;
+class Solution
+{
+public:
+    // function to construct tree from string
+    Node *treeFromString(string str)
+    {
+        stack<Node *> st;
+        int i = 0;
+        int n = str.length();
+        while (i < n)
+        {
+            if (str[i] == '(')
+            {
+                i++;
+            }
+            else if (str[i] == ')')
+            {
+                st.pop();
+                i++;
+            }
+            else
+            {
+                int val = 0;
+                int j = i;
+                while (j < n && (str[j] - '0' >= 0 && str[j] - '0' <= 9))
+                {
+                    val = val * 10 + (str[j] - '0');
+                    j++;
+                }
+
+                Node *next = new Node(val);
+                if (!st.empty())
+                {
+                    if (st.top()->left == NULL)
+                    {
+                        st.top()->left = next;
+                    }
+                    else
+                    {
+                        st.top()->right = next;
+                    }
+                }
+                st.push(next);
+                i = j;
+            }
+        }
+        return st.top();
+    }
+};
