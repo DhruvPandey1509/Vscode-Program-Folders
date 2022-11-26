acc9fe43e3e26e716b2c5f292a31393ff479aa8e
Dhruv Pandey <dhruvpandey1503@gmail.com>
2022-11-26 20:19:36 +0530

    .

--- Geeks for Geeks/Medium/Construct_Binary_Tree_from_String_with_bracket_representation.cpp
+++ Geeks for Geeks/Medium/Construct_Binary_Tree_from_String_with_bracket_representation.cpp
@@ -1,5 +1,17 @@
 #include "bits/stdc++.h"
 using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
+};
 class Solution
 {
 public:
